cb941e00935e4924a2a90472d6e4e52a9766d9ab
Murilocb123 <murilocb1276@gmail.com>
2026-01-22 20:32:22 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Terminal } from './components/terminal/terminal';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Terminal],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
